bd4db23116d40b526fb5ebb4c505d23f80e5e54d
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:20:46 +0100

    Refactor build.gradle.kts to use plugins block

--- build.grade.kts
+++ build.grade.kts
@@ -1,19 +1,14 @@
-buildscript {
-    repositories {
-        mavenCentral()
-        google()
-        maven { url = uri("https://jitpack.io") }
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
+plugins {
+    // On définit les versions des plugins ici sans les appliquer à la racine
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 allprojects {
     repositories {
-        mavenCentral()
         google()
+        mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
 }
