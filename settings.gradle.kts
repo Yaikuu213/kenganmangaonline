38dc255267a2ff49bb9799553060caab1bf15ee0
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:23:01 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
-include(":lib")
-include(":src:en:kenganmanga") // <-- AJOUTE CETTE LIGNE !
-
-project(":lib").projectDir = file("lib")
+include(":src:en:kenganmanga")
 project(":src:en:kenganmanga").projectDir = file("src/en/kenganmanga")
+
+// Ce script va chercher automatiquement toutes les librairies dans le dossier lib
+File(rootDir, "lib").listFiles()?.forEach {
+    if (it.isDirectory && (File(it, "build.gradle.kts").exists() || File(it, "build.gradle").exists())) {
+        include(":lib:${it.name}")
+    }
+}
