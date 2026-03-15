74935ab28a64b3b209782874d3410098bad3f593
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:30:10 +0100

    Update build.grade.kts

--- build.grade.kts
+++ build.grade.kts
@@ -4,6 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+        // C'est ça qui manque au robot !
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
