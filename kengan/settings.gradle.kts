0093fe158bbb24e89d571818022d1f25bc3ca4e8
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:09:30 +0100

    Delete kengan directory

--- kengan/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-include(":lib")
-include(":src:en:kenganmanga") // <-- AJOUTE CETTE LIGNE !
-
-project(":lib").projectDir = file("lib")
-project(":src:en:kenganmanga").projectDir = file("src/en/kenganmanga")
