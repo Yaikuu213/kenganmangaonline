42f1c8d9d47c71350a09a505af3cc59f9c86c0c9
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:36:48 +0100

    Update build.grade.kts

--- build.grade.kts
+++ build.grade.kts
@@ -1,5 +1,4 @@
 plugins {
-    // Ici on prépare les outils sans les activer tout de suite
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
@@ -8,7 +7,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        // Indispensable pour les dépendances Tachiyomi
         maven { url = uri("https://jitpack.io") }
     }
 }
