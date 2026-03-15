c13aa87bc15d43da49531d947c8bf043927eb0c4
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:10:01 +0100

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+include(":lib")
+include(":src:en:kenganmanga") // <-- AJOUTE CETTE LIGNE !
+
+project(":lib").projectDir = file("lib")
+project(":src:en:kenganmanga").projectDir = file("src/en/kenganmanga")
