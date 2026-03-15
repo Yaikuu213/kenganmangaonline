311bdf7259446ddd8f1456c9fe0f0365318d1e25
Yaikuu213 <mohamedemtir66@gmail.com>
2026-03-15 13:34:58 +0100

    Update build.grade.kts

--- build.grade.kts
+++ build.grade.kts
@@ -1,19 +1,14 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // C'est ça qui manque au robot !
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
+plugins {
+    // Ici on prépare les outils sans les activer tout de suite
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+        // Indispensable pour les dépendances Tachiyomi
         maven { url = uri("https://jitpack.io") }
     }
 }
